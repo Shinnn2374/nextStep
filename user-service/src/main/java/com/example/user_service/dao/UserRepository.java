42a9e3bd232844bfae9a69433d4b4306825764fe
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-08-12 13:51:23 +0500

    ...

--- user-service/src/main/java/com/example/user_service/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.user_service.dao;
-
-import com.example.user_service.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface UserRepository extends CrudRepository<User, Long> {
-    User findByEmail(String email);
-}
